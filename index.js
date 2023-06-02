674e86edcbb076b1ed95d3b20c0cefd8f1e807ce
Joshua Tafoya <tafoyajd.91@gmail.com>
2023-06-01 18:20:47 -0700

    filter these functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(array, string) {
+    const matchingDrivers = [];
+    for (const element of array) {
+        if (element.toUpperCase() === string.toUpperCase()) {
+            matchingDrivers.push(element)
+        }
+    }
+    return matchingDrivers;
+}
+
+function fuzzyMatch(array, string) {
+    const fuzzyDrivers = [];
+    for (const element of array) {
+        if (element.startsWith(string)) {
+            fuzzyDrivers.push(element);
+        }
+    }
+    return fuzzyDrivers;
+}
+
+function matchName(array, string) {
+    const matches = [];
+    for (const element of array) {
+        if (element.name === string ) {
+            matches.push(element);
+        }
+    }
+    return matches
+}
